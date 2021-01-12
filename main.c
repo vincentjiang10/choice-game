a64c74b5c4e0030b86a60d260d28c20da247629a
Cullen Ye <59585764+CullenLYe@users.noreply.github.com>
2021-01-12 18:39:52 -0500

    Update main.c

--- main.c
+++ main.c
@@ -7,14 +7,32 @@
 #include <unistd.h>
 #include "functions.h"
 
+int globalint = 1;
+char currentaddress[256]; // To keep track of what node we're currently at.
+
+// FUNCTION TO SAVE THE GAME AT ANY GIVEN POINT.
+void saveGame() {
+  printf("Saved Game!\n");
+  int fd = open("savefile.txt", O_WRONLY);
+  write(fd, currentaddress, sizeof(currentaddress));
+  close(fd);
+}
+
 // function which makes the choice (takes in stdin int)
 int makeChoice(int numChoice) {
     printf("Input choice #: ");
     char choice [10];
     fgets(choice, sizeof(choice), stdin);
-    while (atoi(choice) < 1 || atoi(choice) > numChoice) {
-        printf("Input invalid. Please try again: ");
-        fgets(choice, sizeof(choice), stdin);
+    if (!strcmp(choice, "save\n")) {
+      saveGame();
+      printf("Input choice #: ");
+      fgets(choice, sizeof(choice), stdin);
+    }
+    else {
+      while (atoi(choice) < 1 || atoi(choice) > numChoice) {
+          printf("Input invalid. Please try again: ");
+          fgets(choice, sizeof(choice), stdin);
+      }
     }
     return atoi(choice);
 }
@@ -40,6 +58,7 @@ int reader2(char * address, char * buffer) {
 struct Node makeNode(char str [256], char * buffer, char * buffer2) {
     struct Node node;
     strcpy(node.address, str);
+    strcpy(currentaddress, node.address); // Sets the "currentaddress" (global String) to this node's address.
     reader(node.address, buffer);
     int len = strlen(str);
     if (len == 10) exit(0); // for now it's exit, but we can add a special function (endgame()) that ends the game
@@ -57,18 +76,40 @@ int main() {
     char buffer[5000];
     int fd = open("story.txt", O_RDONLY);
     read(fd, buffer, sizeof(buffer));
-    // loads numChoice.txt into another buffer
     close(fd);
+    // loads numChoice.txt into another buffer
     char buffer2[256];
     int fd2 = open("numChoice.txt", O_RDONLY);
     read(fd2, buffer2, sizeof(buffer2));
     close(fd2);
     //----------------------------------------
     // at the start, check with user whether to load a save file
-    // start at root (new game) if not
-    struct Node root = makeNode("0", buffer, buffer2);
-    // start at the checkpoint if yes
-    // struct Node save = makeNode(address) // address is imported from save file function
-  
+    while (globalint) {
+      printf("Would you like to load in a saved file? (y/n)\n");
+      char buffer3[3];
+      fgets(buffer3, sizeof(buffer3), stdin);
+
+      // IF THE USER INPUTS 'n' AND DOES NOT WISH TO LOAD A SAVE FILE.
+      if (buffer3[0]=='n') { // start at root (new game)
+        globalint=0;
+        char address[10] = "0";
+        struct Node root = makeNode(address, buffer, buffer2);
+      }
+
+      // IF THE USER INPUTS 'y' AND DOES WISH THE LOAD A SAVE FILE.
+      else if (buffer3[0]=='y') { // start at the checkpoint
+        printf("Loading game from saved checkpoint...\n");
+        globalint=0;
+        char address[10];
+        int fd3 = open("savefile.txt", O_RDONLY);
+        read(fd3, address, sizeof(address));
+        close(fd);
+        struct Node save = makeNode(address, buffer, buffer2); // address is imported from save file function
+      }
+
+      else printf("Invalid input. Type 'y' or 'n'.\n");
+
+    }
+
     return 0;
 }
